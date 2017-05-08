9f4d4c529604a4a699d3e7a0b60141e77db76371
Paul Schroeder <paul.schroeder@nimbix.net>
2017-05-08 13:36:09 -0500

    Fix perms..

--- example.sh
+++ example.sh
@@ -8,16 +8,17 @@ if [ -x /usr/sbin/sshd-keygen ]; then
     /usr/sbin/sshd-keygen
 else
     [ ! -f /etc/ssh/ssh_host_key ] && \
-        ssh-keygen -q -f /etc/ssh/ssh_host_key -N '' -t rsa1
+        sudo ssh-keygen -q -f /etc/ssh/ssh_host_key -N '' -t rsa1
     [ ! -f /etc/ssh/ssh_host_rsa_key ] && \
-        ssh-keygen -q -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
+        sudo ssh-keygen -q -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
     [ ! -f /etc/ssh/ssh_host_dsa_key ] && \
-        ssh-keygen -q -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
+        sudo ssh-keygen -q -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
     [ ! -f /etc/ssh/ssh_host_ecdsa_key ] && \
-        ssh-keygen -q -f /etc/ssh/ssh_host_ecdsa_key -N '' -t ecdsa
+        sudo ssh-keygen -q -f /etc/ssh/ssh_host_ecdsa_key -N '' -t ecdsa
     [ ! -f /etc/ssh/ssh_host_ed25519_key ] && \
-        ssh-keygen -q -f /etc/ssh/ssh_host_ed25519_key -N '' -t ed25519
+        sudo ssh-keygen -q -f /etc/ssh/ssh_host_ed25519_key -N '' -t ed25519
 fi
-[ ! -d /var/run/sshd ] && mkdir -p /var/run/sshd
+[ ! -d /var/run/sshd ] && \
+    mkdir -p /var/run/sshd && chown root:root /var/run/sshd
 [ -x /usr/sbin/sshd ] && /usr/sbin/sshd -D
 
