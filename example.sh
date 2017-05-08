43c07460c30bcc0530a6546a7a1eb13f9987e406
Paul Schroeder <paul.schroeder@nimbix.net>
2017-05-08 13:27:31 -0500

    Only create keys if they don't exist..

--- example.sh
+++ example.sh
@@ -7,11 +7,16 @@ fi
 if [ -x /usr/sbin/sshd-keygen ]; then
     /usr/sbin/sshd-keygen
 else
-    ssh-keygen -q -f /etc/ssh/ssh_host_key -N '' -t rsa1
-    ssh-keygen -q -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
-    ssh-keygen -q -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
-    ssh-keygen -q -f /etc/ssh/ssh_host_ecdsa_key -N '' -t ecdsa
-    ssh-keygen -q -f /etc/ssh/ssh_host_ed25519_key -N '' -t ed25519
+    [ ! -f /etc/ssh/ssh_host_key ] && \
+        ssh-keygen -q -f /etc/ssh/ssh_host_key -N '' -t rsa1
+    [ ! -f /etc/ssh/ssh_host_rsa_key ] && \
+        ssh-keygen -q -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
+    [ ! -f /etc/ssh/ssh_host_dsa_key ] && \
+        ssh-keygen -q -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
+    [ ! -f /etc/ssh/ssh_host_ecdsa_key ] && \
+        ssh-keygen -q -f /etc/ssh/ssh_host_ecdsa_key -N '' -t ecdsa
+    [ ! -f /etc/ssh/ssh_host_ed25519_key ] && \
+        ssh-keygen -q -f /etc/ssh/ssh_host_ed25519_key -N '' -t ed25519
 fi
 [ ! -d /var/run/sshd ] && mkdir -p /var/run/sshd
 [ -x /usr/sbin/sshd ] && /usr/sbin/sshd -D
